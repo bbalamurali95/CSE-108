213315ce0a340b448919ca591e26ec39d45e1518
Bailey McCalmont <bmcmoose@gmail.com>
2026-05-10 22:34:17 -0700

    test commit

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy(app)
 
 class User(db.Model):
@@ -17,3 +20,8 @@ def index():
     return render_template("home.html")
 
 bcrypt = Bcrypt(app)
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
