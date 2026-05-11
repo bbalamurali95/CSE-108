9653080a7bd4bae8a7456a1b92d0bc5bc8c049f4
Justin Luis Guevarra <geslaniluis@gmail.com>
2026-05-10 21:35:16 -0700

    Added route for homepage

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
 from flask_bcrypt import Bcrypt
-from flask import request, jsonify
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("frontpage.html")
 
 bcrypt = Bcrypt(app)
