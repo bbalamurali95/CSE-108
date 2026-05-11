0f7b3ba5421294034fa03319e77c6057dac15921
Justin Luis Guevarra <geslaniluis@gmail.com>
2026-05-11 16:46:22 -0700

    Added createTournament

--- static/script.js
+++ static/script.js
@@ -37,15 +37,28 @@ function handleLogin(event) {
         console.log("Status: ", this.status);
         //console.log("Responsse: ", this.data);
 
-
         if (this.status === 200) {
             window.location.href = "/";
         } else {
             alert(this.responseText);
         }
     }
+}
 
-    
+function createTournament(event) {
+    event.preventDefault();
+
+    const xhttp = new XMLHttpRequest();
+    const tourneyName = document.getElementById("tourneyNameBox").value
+    const tourneyUrl = document.getElementById("tourneyUrlBox").value
+    const body = {"name": tourneyName, "url": tourneyUrl};
+    xhttp.open("POST", "/create_tournament", true);
+    xhttp.sendRequestHeader("Content-Type", "application/json");
+    xhttp.send(JSON.stringify(body));
+    xhttp.onload = function() {
+        if (this.status === 201) window.location.reload();
+        else console.error("Failed to create tournament");
+    }
 }
 
 document.addEventListener('DOMContentLoaded', function () {
