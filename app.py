7e2a8e558926d6fe641d5c5ff78346162d28b2fa
Bailey McCalmont <bmcmoose@gmail.com>
2026-05-10 21:57:01 -0700

    user database

--- app.py
+++ app.py
@@ -3,6 +3,12 @@ from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True, nullable=False)
+    salt = db.Column(db.String, nullable=False)
+    passwordHash = db.Column(db.String, nullable=False)
+
 @app.route('/')
 def index():
     return render_template("home.html")
