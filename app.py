347dcfd681fddab7840f45eca1289a7d4e233e37
Justin Luis Guevarra <geslaniluis@gmail.com>
2026-05-10 22:46:10 -0700

    Created register route

--- app.py
+++ app.py
@@ -5,15 +5,35 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, unique=True, nullable=False)
-    salt = db.Column(db.String, nullable=False)
     password_hash = db.Column(db.String, nullable=False)
 
-@app.route('/')
+@app.route("/")
 def index():
     return render_template("home.html")
+
+@app.route("/register", methods=['POST'])
+def register():
+    new_data = request.get_json()
+    username = new_data.get("username")
+    password = new_data.get("password")
+
+    hashed_pw  = bcrypt.generate_password_hash(\
+        password
+    ).decode("utf-8")
+
+    if User.query.filter_by(username = username).first():
+        if username and password:
+            return jsonify({"error" : "User already exists"}), 400
+        user = User(username = username, password_hash = password)
+        db.session.add(user)
+        db.session.commit()
+        return jsonify({"message" : f"User {username} created successfully"}), 201
+    else:
+        return jsonify({"error" : "Missing username or password"}), 400
 
 bcrypt = Bcrypt(app)
