12297fadee26c2bfb64a7eb09527b90d130792c3
An Actual g00se <bmcmoose@gmail.com>
2026-05-12 01:52:22 -0700

    testing database url

--- app.py
+++ app.py
@@ -11,7 +11,12 @@ app = Flask(__name__)
 
 load_dotenv()
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("postgresql://postgres:mpsZZlmhGbpBlaANcxpSImVGHfqPtRuY@postgres.railway.internal:5432/railway")
+database_url = os.getenv('DATABASE_URL')
+if database_url and database_url.startswith('postgres://'):
+    database_url = database_url.replace('postgres://', 'postgresql://', 1)
+app.config['SQLALCHEMY_DATABASE_URI'] = database_url
+
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("postgresql://postgres:mpsZZlmhGbpBlaANcxpSImVGHfqPtRuY@postgres.railway.internal:5432/railway")
 app.config['JWT_SECRET_KEY'] = os.getenv("JWT_SECRET_KEY")
 app.config['JWT_TOKEN_LOCATION'] = ['cookies']
 app.config['JWT_COOKIE_CSRF_PROTECT'] = False
