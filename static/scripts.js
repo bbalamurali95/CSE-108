16aa0566ce4a7c2bf5c1c70b5ce90016200336ae
Justin Luis Guevarra <geslaniluis@gmail.com>
2026-05-11 21:08:56 -0700

    removed hardcoded player wins dictionary

--- static/scripts.js
+++ static/scripts.js
@@ -1,30 +1,3 @@
-let players = [
-    { name: "John", game: "Tekken", wins: 8 },
-    { name: "Ryan", game: "Guilty Gear", wins: 7 },
-    { name: "Sam", game: "Street Fighter", wins: 6 },
-    { name: "Kevin", game: "Tekken", wins: 5 },
-    { name: "Alex", game: "Guilty Gear", wins: 5 },
-    { name: "Daniel", game: "Street Fighter", wins: 4 },
-    { name: "Jordan", game: "Tekken", wins: 4 },
-    { name: "Chris", game: "Street Fighter", wins: 3 },
-    { name: "Marcus", game: "Guilty Gear", wins: 3 },
-    { name: "Leo", game: "Tekken", wins: 3 },
-    { name: "Nate", game: "Street Fighter", wins: 2 },
-    { name: "Evan", game: "Guilty Gear", wins: 2 },
-    { name: "Victor", game: "Tekken", wins: 2 },
-    { name: "Adrian", game: "Street Fighter", wins: 2 },
-    { name: "Noah", game: "Tekken", wins: 2 },
-    { name: "Eli", game: "Guilty Gear", wins: 2 },
-    { name: "Omar", game: "Street Fighter", wins: 1 },
-    { name: "Jay", game: "Guilty Gear", wins: 1 },
-    { name: "Mason", game: "Tekken", wins: 1 },
-    { name: "Luis", game: "Street Fighter", wins: 1 },
-    { name: "Tyler", game: "Tekken", wins: 1 },
-    { name: "Brandon", game: "Guilty Gear", wins: 1 },
-    { name: "Eric", game: "Street Fighter", wins: 0 },
-    { name: "Sean", game: "Tekken", wins: 0 }
-];
-
 function loadLeaderboard() {
     let selectedGame = document.getElementById("gameSelect").value;
     let tableBody = document.getElementById("leaderboardBody");
